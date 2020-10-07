e59c6830a01ad74e5811aeac5bd80206ba372e0d
lagofelipe <lagofelipe@gmail.com>
2020-10-07 15:57:51 -0500

    done

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,24 @@
-require 'pry'
+#require 'pry'
 
 class String
 
   def sentence?
-    
+    self.end_with?('.')
   end
 
   def question?
-
+    self.end_with?('?')
   end
 
   def exclamation?
-
+    self.end_with?('!')
   end
 
-  def count_sentences
 
+  # split(/\.|\?|!!/)
+  def count_sentences
+   counter = self.split(/\.|\?|!!/).length
+  
+   #the // are the delimiters at the beginning and end, so they’re not matching anything. When you see an escape \character, it’s usually to say that a character that has a special meaning in regex (like .  or ?) is the actual character. And the |is an “or”, so that’s saying to match a period, a question mark, or two exclamations.
   end
 end
